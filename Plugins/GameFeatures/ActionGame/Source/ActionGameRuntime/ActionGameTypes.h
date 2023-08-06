a8c811fe44d3caa13248d6d8c64fc821033f33be
Matt-Makes <15056270085@163.com>
2023-08-06 15:05:38 +0800

    Ready Mantle CMC

---
+++ Plugins/GameFeatures/ActionGame/Source/ActionGameRuntime/ActionGameTypes.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ActionGameTypes.generated.h"
+
+class UGameplayAbility;
+class UGameplayEffect;
+class UGameplayAbility;
+class UAnimMontage;
+class UNiagaraSystem;
+
+UENUM(BlueprintType)
+enum class EMovementDirectionType : uint8
+{
+	None  UMETA(DisplayName = "None"),
+	OrientToMovement  UMETA(DisplayName = "OrientToMovement"),
+	Strafe  UMETA(DisplayName = "Strafe")
+};
---
+++ Plugins/GameFeatures/ActionGame/Source/ActionGameRuntime/Character/RDCharacterMovementComponent2.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RDCharacterMovementComponent2.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"QA	
+
+bool URDCharacterMovementComponent2::TryTraversal(UAbilitySystemComponent* ASC)
+{
+}
+
+void URDCharacterMovementComponent2::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+EMovementDirectionType URDCharacterMovementComponent2::GetMovementDirectionType() const
+{
+}
+
+void URDCharacterMovementComponent2::SetMovementDirectionType(EMovementDirectionType InMovementDirectionType)
+{
+}
+
+void URDCharacterMovementComponent2::OnEnforcedStrafeTagChanged(const FGameplayTag CallbackTag, int32 NewCount)
+{
+}
+
+void URDCharacterMovementComponent2::HandleMovementDirection()
+{
+}
