86983ff0ba7847357c1b6355f809b0c1ed1447b1
Matt-Makes <15056270085@163.com>
2023-09-12 09:35:28 +0800

    display CharacterOverlay UI messages

---
+++ Source/Bong/HUD/CharacterOverlay.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CharacterOverlay.generated.h"
+
+
+class UProgressBar;
+class UTextBlock;
+
+UCLASS()
+class BONG_API UCharacterOverlay : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UCharacterOverlay(const FObjectInitializer& ObjectInitializer);
+
+	
+	UPROPERTY(meta = (BindWidget))
+	UProgressBar* HealthBar;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* HealthText;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* ScoreAmount;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* DefeatsAmount;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* WeaponAmmoAmount;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* CarriedAmmoAmount;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* MatchCountdownText;
+};
