0056f83e939d21d53cdfe20ecccf3083a385c190
Matt-Makes <15056270085@163.com>
2023-09-20 09:54:22 +0800

    fix file lack

---
+++ Source/BlueprintToCpp/Public/FQuestInfo.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "FQuestInfo.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct BLUEPRINTTOCPP_API FQuestInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName QuestID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Progress;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 ProgressTotal;
+};
