de3f6da16dee907795faf77ee6a1dbc2cc656d66
Matt-Makes <15056270085@163.com>
2024-07-29 14:08:44 +0800

    Add PlayerMoveTo Plugin

---
+++ Plugins/PlayerMoveTo/Source/PlayerMoveTo/Private/PlayerAIMoveToController.cpp
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Jared Taylor. All Rights Reserved
+
+
+#include "PlayerAIMoveToController.h"
+
+#include "GameplayTasksComponent.h"
+#include "GameFramework/Pawn.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(PlayerAIMoveToController)
+
+UGameplayTasksComponent* APlayerAIMoveToController::GetGameplayTasksComponent(const UGameplayTask& Task) const
+{
+	return GetGameplayTasksComponent();
+}
+
+AActor* APlayerAIMoveToController::GetGameplayTaskOwner(const UGameplayTask* Task) const
+{
+	return const_cast<APlayerAIMoveToController*>(this);
+}
+
+AActor* APlayerAIMoveToController::GetGameplayTaskAvatar(const UGameplayTask* Task) const
+{
+	return GetPawn();
+}
+
+uint8 APlayerAIMoveToController::GetGameplayTaskDefaultPriority() const
+{
+	return FGameplayTasks::DefaultPriority - 1;
+}
+
+void APlayerAIMoveToController::CacheGameplayTasksComponent(APawn* InPawn)
+{
+	// a Pawn controlled by AI _requires_ a GameplayTasksComponent, so if Pawn 
+	// doesn't have one we need to create it
+	if (CachedGameplayTasksComponent == nullptr)
+	{
+		UGameplayTasksComponent* GTComp = InPawn->FindComponentByClass<UGameplayTasksComponent>();
+		if (GTComp == nullptr)
+		{
+			GTComp = NewObject<UGameplayTasksComponent>(InPawn, TEXT("GameplayTasksComponent"));
+			GTComp->RegisterComponent();
+		}
+		CachedGameplayTasksComponent = GTComp;
+	}
+}
+
+void APlayerAIMoveToController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	CacheGameplayTasksComponent(InPawn);
+}
---
+++ Plugins/PlayerMoveTo/Source/PlayerMoveTo/Public/PlayerAIMoveToController.h
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Jared Taylor. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTaskOwnerInterface.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerAIMoveToController.generated.h"
+
+/**
+ * This PlayerController is required when using UPlayerAITask_MoveTo, but not UGameplayPlayerAITask_MoveTo
+ * If you're using only with gameplay abilities, it is not needed
+ */
+UCLASS(config=Game, BlueprintType, Blueprintable)
+class PLAYERMOVETO_API APlayerAIMoveToController : public APlayerController, public IGameplayTaskOwnerInterface
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	TObjectPtr<UGameplayTasksComponent> CachedGameplayTasksComponent;
+	
+	//----------------------------------------------------------------------//
+	// IGameplayTaskOwnerInterface
+	//----------------------------------------------------------------------//
+	virtual UGameplayTasksComponent* GetGameplayTasksComponent(const UGameplayTask& Task) const override;
+	virtual AActor* GetGameplayTaskOwner(const UGameplayTask* Task) const override;
+	virtual AActor* GetGameplayTaskAvatar(const UGameplayTask* Task) const override;
+	virtual uint8 GetGameplayTaskDefaultPriority() const override;
+
+	FORCEINLINE UGameplayTasksComponent* GetGameplayTasksComponent() const { return CachedGameplayTasksComponent; }
+
+public:
+	void CacheGameplayTasksComponent(APawn* InPawn);
+	virtual void OnPossess(APawn* InPawn) override;
+};
